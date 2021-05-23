9f5fd01ac1e749c36101b09c420411a75a383976
Viral <vaidyaviral@gmail.com>
2021-05-23 15:51:25 -0500

    Updated code. toString

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,6 +1,6 @@
 package org.launchcode.techjobs_oo;
 
-import java.util.Objects;
+import java.util.*;
 
 public class Job {
 
@@ -94,4 +94,54 @@ public class Job {
     public void setCoreCompetency(CoreCompetency coreCompetency) {
         this.coreCompetency = coreCompetency;
     }
+
+
+    @Override
+    public String toString() {
+        String noData = "Data not available";
+        String finalString = "";
+
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+
+        HashMap<String, String> idField = new HashMap<String, String>();
+        idField.put("ID :", Integer.toString(this.id));
+        list.add(idField);
+
+        HashMap<String, String> nameFields = new HashMap<String, String>();
+        nameFields.put("Name :", this.getName() != null ?this.getName():noData);
+        list.add(nameFields);
+
+        HashMap<String, String> empFields = new HashMap<String, String>();
+        empFields.put("Employer :", this.employer.toString() != null?this.employer.toString():noData);
+        list.add(empFields);
+
+        HashMap<String, String> locationFields = new HashMap<String, String>();
+        locationFields.put("Location :", this.location.toString() != null?this.location.toString():noData);
+        list.add(locationFields);
+
+        HashMap<String, String> positionTypeFields = new HashMap<String, String>();
+        positionTypeFields.put("Position Type :", this.positionType.toString()!= null?this.positionType.toString():noData);
+        list.add(positionTypeFields);
+
+        HashMap<String, String> coreCompetencyFields = new HashMap<String, String>();
+        coreCompetencyFields.put("Core Competency :", this.getCoreCompetency().toString()!= null?this.getCoreCompetency().toString():noData);
+        list.add(coreCompetencyFields);
+
+
+
+        for (int i = 0; i < list.size(); i++) {
+            HashMap<String, String> data = list.get(i);
+            finalString =  finalString + "\n";
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                finalString =  finalString + key + value;
+            }
+        }
+
+        return  finalString;
+    }
+
+
+
 }
--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,7 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
-import static org.junit.Assert.assertFalse;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
 
 public class JobTest {
 
@@ -17,6 +18,66 @@ public class JobTest {
     @Test
     public void testSettingJobId() {
         assertFalse(Job1.getId() == Job2.getId());
+        assertEquals(Job2.getId()-Job1.getId(),1);
     }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+
+        Job jbFields = new Job("Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        //checking each property of an object set correctly through each object constructor
+        assertEquals(jbFields.getName(), "Product tester");
+        assertEquals(jbFields.getEmployer().getValue(), "ACME");
+        assertEquals(jbFields.getCoreCompetency().getValue(), "Persistence");
+        assertEquals(jbFields.getPositionType().getValue(), "Quality control");
+        assertEquals(jbFields.getLocation().getValue(), "Desert");
+
+        //checking each property of an object set correctly through each object constructor
+        assertTrue(jbFields.getEmployer() instanceof Employer);
+        assertTrue(jbFields.getLocation() instanceof Location);
+        assertTrue(jbFields.getPositionType() instanceof PositionType);
+        assertTrue(jbFields.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job cvsJob1 = new Job("ABC",new Employer("CVS"),new Location("STL"),new PositionType("Pharmacist"),
+                new CoreCompetency("chemist"));
+        Job cvsJob2 = new Job("ABC",new Employer("CVS"),new Location("STL"),new PositionType("Pharmacist"),
+                new CoreCompetency("chemist"));
+
+        //Check if cvsJob1 id is not equal to cvsJob2 id
+        assertFalse(cvsJob1.getId() == cvsJob2.getId());
+
+        //Check if same object pass in equal
+        assertTrue(cvsJob1.equals(cvsJob1));
+        //check if cvsJob1 and cvsJob2 are not same
+        assertFalse(cvsJob1.equals(cvsJob2));
+    }
+
+    @Test
+    public void testNewLines() {
+        Job cvsJob1 = new Job("ABC",new Employer("CVS"),
+                new Location("STL"),new PositionType("Pharmacist"),
+                new CoreCompetency("chemist"));
+         assertTrue(cvsJob1.toString().contains("ABC\n"));
+         assertTrue(cvsJob1.toString().contains("STL\n"));
+    }
+
+    @Test
+    public void testEmptyData() {
+        //No Employer pass
+        Job cvsJob1 = new Job("ABC",new Employer(),
+                new Location("STL"),new PositionType("Pharmacist"),
+                new CoreCompetency("chemist"));
+        assertTrue(cvsJob1.toString().contains("Employer :Data not available\n"));
+    }
+
+
+
 
 }
